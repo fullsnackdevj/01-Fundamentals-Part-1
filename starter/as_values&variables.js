50e319330e487241e8ca2f101f968d142d5eb212
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-08-01 10:07:13 +0800

    adding first assignment in this course

---
+++ starter/as_values&variables.js
@@ -0,0 +1,12 @@
+// Values and Variables
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// Log their values to the console.
+
+let country = "Philippines";
+let continent = "Asia";
+let population = "33.5 Billion";
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,25 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jay";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// camelCaseNotation
+
+// how to name variables ******
+
+// > variable must not start with a number
+
+// variable names can only contain numbers, letters, underscores or dollar sign.
+
+// don't name variables using reserve keywords
+
+// start by writing a lowercase on the first Letter. its not illegal to use Uppercase in the first letter but they are specific use cases that we need to uppercase the first letter in OOP
+
+// if its constants write them in uppercase ie: let PI = 3.1415;

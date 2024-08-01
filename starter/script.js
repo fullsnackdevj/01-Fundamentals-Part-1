190f5d9381b89a6474b35ac6691112122e9c7207
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-08-01 11:01:54 +0800

    added a line for 'LECTURE ABOUT DATA TYPES STARTS '

--- starter/script.js
+++ starter/script.js
@@ -1,14 +1,14 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Jay";
+// let firstName = "Jay";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
 // camelCaseNotation
 
@@ -23,3 +23,30 @@ console.log(firstName);
 // start by writing a lowercase on the first Letter. its not illegal to use Uppercase in the first letter but they are specific use cases that we need to uppercase the first letter in OOP
 
 // if its constants write them in uppercase ie: let PI = 3.1415;
+
+// LECTURE ABOUT DATA TYPES STARTS HERE
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun);
+
+// changing data type of variables on fly called 'dynamic typing' ^
+
+// sample of undefined data type:
+let year; //variable is declared but no declared value.
+console.log(year); //value will be 'undefined'
+console.log(typeof year); // data type will be 'undefined'
+
+year = 1991; //declaring variable without 'let' because its already declared above.
+console.log(typeof year); // dynamic typing from 'undefined' to 'number' result = 'number'
+
+console.log(typeof null); // bug, bec. null is not an object.
+
+// LECTURE ABOUT DATA TYPES ENDS HERE

b375493fe05039c7677762ae67b51e9ece467699
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-08-23 00:56:02 +0800

    updated if / else statement

--- starter/if_else_statements.js
+++ starter/if_else_statements.js
@@ -33,11 +33,10 @@ const heightJohn = 1.76;
 
 const BMIMark = massMark / (heightMark * heightMark);
 const BMIJohn = massJohn / (heightJohn * heightJohn);
-const higherBMI = BMIMark > BMIJohn;
 
 /* Write your code below. Good luck! 🙂 */
 
-if (higherBMI) {
+if (BMIMark > BMIJohn) {
   console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
 } else {
   console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);

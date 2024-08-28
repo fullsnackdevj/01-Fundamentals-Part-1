67dcf7b36618e081b7fa2ce2751aee70221ae937
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-08-28 17:45:11 +0800

    boolean logic notes

---
+++ starter/boolean_logic.js
@@ -0,0 +1,14 @@
+//notes
+/* '
+
+The 'And' operator 
+'true when all are true
+'false when one is false
+
+The 'Or' operator
+'true when ONE is true even there are false statements
+'false when all are false. // only situation were 'OR' gets false.
+
+The 'NOT' operator
+'inverts true / false value.
+

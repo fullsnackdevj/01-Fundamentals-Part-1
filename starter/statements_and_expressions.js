3ad6a58cb66ffb5078373e71dab417fba17f6da3
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-08-29 13:06:12 +0800

    added additional notes to statements and expressions lecture

--- starter/statements_and_expressions.js
+++ starter/statements_and_expressions.js
@@ -1,1 +1,24 @@
 //notes for statements and Expressions
+
+/* 
+Expression is a piece of code that produces a value.
+
+ie: 3 + 4 is an expression, because it does produce value.
+
+1991 is also an expression .
+
+true && false && !false is also an expression. 
+
+*/
+
+// Statement is a bigger piece of code that is executed and which does not produce a value on itself.
+
+if (23 > 10) {
+  const str = "23 is bigger";
+}
+
+// expression = produces value
+// statements = does not produce value
+
+// 'Statements are like full sentences that translate our actions.
+// the actions that we want the program to perform.
